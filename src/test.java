e011d01703482ce62988e61ed299ee0cea71a3ad
Rich Baird <rich@Richs-MacBook-Air.local>
2016-04-18 15:06:17 -0600

    Update tests to test for new methods

--- src/test.java
+++ src/test.java
@@ -5,9 +5,10 @@
  ******************************************/
 public class test {
     public static void main(String[] args) {
-        battleship = new Battleship();
-        while(!battleship.winner) {
-            battleship.nextTurn();
-        }
+        //System.out.print(Ship.wasShipHit(Ship.Ships.AIRCRAFT,"A6","A1","E"));
+        Player player1 = new Player("player1", Player.playerType.COMPUTER,1);
+        Player player2 = new Player("player1", Player.playerType.HUMAN,2);
+        Battleship battleship = new Battleship(player1,player2);
+        battleship.nextTurn();
     }
 }
