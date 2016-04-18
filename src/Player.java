fbce2e349e0e657fa1ad10a3e75cae00da7226d2
Rich Baird <rich@Richs-MacBook-Air.local>
2016-04-18 15:07:18 -0600

    Assign name number and type when creating new player

--- src/Player.java
+++ src/Player.java
@@ -1,61 +1,57 @@
-/**Daniel Humeniuk
+/**
+ * Daniel Humeniuk
  * CSIS 1400 5:00p.m.-6:50p.m. MW
  * Group Project
  */
 public class Player {
 
-        public enum playerType{HUMAN, COMPUTER}
-        public String name;
-        private int wins;
-        private int score;
-        private playerType type;
-        private int playerNumber;
+    public enum playerType {HUMAN, COMPUTER}
 
-        public Player(String name, int PlayerType, int playerNumber)
-        {
+    public String name;
+    private int wins;
+    private int score;
+    private playerType type;
+    private int playerNumber;
 
-        }
+    public Player(String name, playerType type, int playerNumber) {
+        this.name = name;
+        this.type = type;
+        this.playerNumber = playerNumber;
+    }
 
-        public String getName()
-        {
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public int getWins()
-        {
-            return wins;
-        }
+    public int getWins() {
+        return wins;
+    }
 
-        public int getScore()
-        {
-            return score;
-        }
+    public int getScore() {
+        return score;
+    }
 
-        public void setName()
-        {
-            this.name = name;
-        }
+    public void setName() {
+        this.name = name;
+    }
 
-        public void setWins()
-        {
-            this.wins = wins;
-        }
+    public void setWins() {
+        this.wins = wins;
+    }
 
-        public void setScore()
-        {
-            this.score = score;
-        }
+    public void setScore() {
+        this.score = score;
+    }
 
-        public void setPlayerNumber()
-        {
-            this.playerNumber = playerNumber;
-        }
+    public void setPlayerNumber() {
+        this.playerNumber = playerNumber;
+    }
 
-        public int getPlayerNumber()
-        {
-            return playerNumber;
-        }
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
 
-
-
+    public playerType getType() {
+        return type;
+    }
 }
